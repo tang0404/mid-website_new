1c05e1ef01e61d2ad40d20dfd74a461efd2345bc
tang0404 <91815841+tang0404@users.noreply.github.com>
2021-11-09 13:17:31 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('.slideshow').each(function(){
+    
+    let slideImgs = $(this).find('img'),
+        slideImgsCount = slideImgs.length,
+        currentIndex = 0;
+    
+    slideImgs.eq(currentIndex).fadeIn();
+    
+    setInterval(showNextSlide, 3000);
+    
+    function showNextSlide(){
+        let nextIndex = (currentIndex + 1) % slideImgsCount;
+        console.log(nextIndex)
+        slideImgs.eq(currentIndex).fadeOut();
+        slideImgs.eq(nextIndex).fadeIn();
+        currentIndex = nextIndex;
+    }
+})
+
